caab4855c439ae394f2c61b759b1b44730f99bc3
Tomasz Sterna <tomek@xiaoka.com>
2014-07-06 09:34:43 +0200

    Moved DBus service related properties to DBusProxy class

--- daemon/manager.h
+++ daemon/manager.h
@@ -18,10 +18,8 @@ using namespace CommHistory;
 class Manager : public QObject
 {
     Q_OBJECT
-    Q_PROPERTY(QVariantMap pebble READ pebble NOTIFY pebbleChanged)
-    Q_PROPERTY(QString name READ pebbleName NOTIFY pebbleChanged)
-    Q_PROPERTY(QString address READ pebbleAddress NOTIFY pebbleChanged)
-    Q_PROPERTY(bool connected READ pebbleConnected NOTIFY connectedChanged)
+
+    friend class DBusProxy;
 
     QBluetoothLocalDevice btDevice;
 
@@ -41,14 +39,7 @@ public:
     Q_INVOKABLE QString findPersonByNumber(QString number);
     Q_INVOKABLE void processUnreadMessages(GroupObject *group);
 
-    QVariantMap pebble() { return dbus->pebble(); }
-    QString pebbleName() { return dbus->pebble()["Name"].toString(); }
-    QString pebbleAddress() { return dbus->pebble()["Address"].toString(); }
-    bool pebbleConnected() { return watch->isConnected(); }
-
 signals:
-    void pebbleChanged();
-    void connectedChanged();
 
 public slots:
     void hangupAll();
@@ -61,6 +52,24 @@ protected slots:
     void onActiveVoiceCallStatusChanged();
     void onConversationGroupAdded(GroupObject *group);
     void onUnreadMessagesChanged();
+
+};
+
+class DBusProxy : public QObject
+{
+    Q_OBJECT
+    Q_PROPERTY(QVariantMap pebble READ pebble)
+    Q_PROPERTY(QString name READ pebbleName)
+    Q_PROPERTY(QString address READ pebbleAddress)
+    Q_PROPERTY(bool connected READ pebbleConnected)
+
+    QVariantMap pebble() { return static_cast<Manager*>(parent())->dbus->pebble(); }
+    QString pebbleName() { return static_cast<Manager*>(parent())->dbus->pebble()["Name"].toString(); }
+    QString pebbleAddress() { return static_cast<Manager*>(parent())->dbus->pebble()["Address"].toString(); }
+    bool pebbleConnected() { return static_cast<Manager*>(parent())->watch->isConnected(); }
+
+public:
+    explicit DBusProxy(QObject *parent) : QObject(parent) {}
 
 };
 
