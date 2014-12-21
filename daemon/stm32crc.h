8f0905197f7cead299c00006ada482095fe9d1a4
Javier <dev.git@javispedro.com>
2014-12-21 18:25:06 +0100

    allow buffer sizes that are not multiples of 4 in crc calculation

--- daemon/stm32crc.h
+++ daemon/stm32crc.h
@@ -10,8 +10,6 @@ public:
 
     void reset();
 
-    // Data size must be multiple of 4, data must be aligned to 4.
-
     void addData(const char *data, int length);
     void addData(const QByteArray &data);
 
@@ -19,6 +17,8 @@ public:
 
 private:
     quint32 crc;
+    quint8 buffer[4];
+    quint8 rem;
 };
 
 #endif // STM32CRC_H
