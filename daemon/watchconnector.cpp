0ad6b1fc702b56bed96077d540366a9a96903da7
Andrew Branson <andrew.branson@cern.ch>
2015-04-21 12:47:07 +0200

    Check if Bluetooth is on before reconnecting
    Don't try to reconnect to the watch if Bluetooth is switched off,
    instead start an extended reconnect timer (which has been refactored)

--- daemon/watchconnector.cpp
+++ daemon/watchconnector.cpp
@@ -158,6 +158,16 @@ void WatchConnector::handleWatch(const QString &name, const QString &address)
 {
     qCDebug(l) << "handleWatch" << name << address;
     reconnectTimer.stop();
+
+    // Check if bluetooth is on
+    QBluetoothLocalDevice host;
+    bool btOff = host.hostMode() == QBluetoothLocalDevice::HostPoweredOff;
+    if (btOff) {
+        qCDebug(l) << "Bluetooth switched off.";
+        setIncreasedReconnectTimer();
+        return;
+    }
+
     if (socket != nullptr) {
         socket->close();
         socket->deleteLater();
@@ -323,6 +333,12 @@ void WatchConnector::onDisconnected()
         writeData.clear(); // 3rd time around - user is not here, do not bother with resending last message
     }
 
+    setIncreasedReconnectTimer();
+
+}
+
+void WatchConnector::setIncreasedReconnectTimer()
+{
     if (reconnectTimer.interval() < 10 * RECONNECT_TIMEOUT) {
         reconnectTimer.setInterval(reconnectTimer.interval() + RECONNECT_TIMEOUT);
     }
