93e793293750b8df69fc1c9374fd29f626bba10e
Kees <kkoenders@xs4all.nl>
2015-06-26 23:58:53 +0200

    Create pebble-nl_NL.ts
    Added Dutch translations

---
+++ app/translations/pebble-nl_NL.ts
@@ -0,0 +1,480 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="31"/>
+        <source>Version</source>
+        <translation>Versie</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="43"/>
+        <source>All Rights Reserved.</source>
+        <translation>Alle rechten voorbehouden</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="67"/>
+        <source>Support</source>
+        <translation>Bijdrage</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="81"/>
+        <source>Your donations help justify development time.</source>
+        <translation>Uw donaties maken ontwikkeltijd gerechtvaardigd.</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="94"/>
+        <source>Thank you for your support!!!</source>
+        <translation>Bedankt voor uw bijdrage!!!</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="97"/>
+        <source>PayPal Donate</source>
+        <translation>PayPal donatie</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="107"/>
+        <source>Bugs?</source>
+        <translation>Bugs?</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="114"/>
+        <source>Open Bug Tracker</source>
+        <translation>Bug Tracker openen</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="123"/>
+        <source>Forum Thread</source>
+        <translation>Forum thread</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="132"/>
+        <source>Send issue e-mail to developer</source>
+        <translation>Issue naar ontwikkelaar mailen</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="141"/>
+        <source>Debug Mode</source>
+        <translation>Debug modus</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="142"/>
+        <source>Enable daemon and app debugging</source>
+        <translation>Debuggen van daemon en app activeren</translation>
+    </message>
+</context>
+<context>
+    <name>AppConfigDialog</name>
+    <message>
+        <location filename="../qml/pages/AppConfigDialog.qml" line="75"/>
+        <source>No configuration settings available</source>
+        <translation>Geen configuratie-instellingen aanwezig</translation>
+    </message>
+</context>
+<context>
+    <name>AppStorePage</name>
+    <message>
+        <location filename="../qml/pages/AppStorePage.qml" line="27"/>
+        <source>Logout</source>
+        <translation>Afmelden</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AppStorePage.qml" line="29"/>
+        <source>Logging out...</source>
+        <translation>Bezig met afmelden...</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AppStorePage.qml" line="36"/>
+        <source>Hide search</source>
+        <translation>Zoeken verbergen</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AppStorePage.qml" line="36"/>
+        <source>Show search</source>
+        <translation>Zoeken tonen</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AppStorePage.qml" line="49"/>
+        <source>Pebble Appstore</source>
+        <translation>Pebble Appstore</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AppStorePage.qml" line="78"/>
+        <source>Apps</source>
+        <translation>Apps</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AppStorePage.qml" line="86"/>
+        <source>Faces</source>
+        <translation>Faces</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AppStorePage.qml" line="109"/>
+        <source>Downloading...</source>
+        <translation>Bezig met downloaden...</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AppStorePage.qml" line="141"/>
+        <source>Downloading %1...</source>
+        <translation>Bezig met downloaden %1...</translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <location filename="../qml/cover/CoverPage.qml" line="56"/>
+        <source>connected</source>
+        <translation>verbonden</translation>
+    </message>
+    <message>
+        <location filename="../qml/cover/CoverPage.qml" line="56"/>
+        <source>disconnected</source>
+        <translation>niet verbonden</translation>
+    </message>
+</context>
+<context>
+    <name>Disclaimer</name>
+    <message>
+        <location filename="../qml/pages/Disclaimer.qml" line="28"/>
+        <source>Feature unavailable</source>
+        <translation>Optie niet beschikbaar</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Disclaimer.qml" line="38"/>
+        <source>This feature is available for supporters only.</source>
+        <translation>Deze optie is alleen beschikbaar voor bijdragers.</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Disclaimer.qml" line="51"/>
+        <source>Supporter?</source>
+        <translation>Bijdragen?</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Disclaimer.qml" line="58"/>
+        <source>Send me my code!</source>
+        <translation>Stuur mij mijn code!</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Disclaimer.qml" line="68"/>
+        <source>Activation code</source>
+        <translation>Activeringscode</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Disclaimer.qml" line="41"/>
+        <source>PayPal Donate</source>
+        <translation>PayPal donatie</translation>
+    </message>
+</context>
+<context>
+    <name>InstallAppDialog</name>
+    <message>
+        <location filename="../qml/pages/InstallAppDialog.qml" line="18"/>
+        <source>Select App files</source>
+        <translation>App bestanden selecteren</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/InstallAppDialog.qml" line="65"/>
+        <source>Install App</source>
+        <translation>App installeren</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/InstallAppDialog.qml" line="66"/>
+        <source>Install</source>
+        <translation>Installeren</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/InstallAppDialog.qml" line="73"/>
+        <source>Add App file</source>
+        <translation>App-bestand toevoegen</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/InstallAppDialog.qml" line="143"/>
+        <source>Delete App file</source>
+        <translation>App-bestand verwijderen</translation>
+    </message>
+</context>
+<context>
+    <name>ManagerPage</name>
+    <message>
+        <location filename="../qml/pages/ManagerPage.qml" line="56"/>
+        <source>Pebble Appstore</source>
+        <translation>Pebble Appstore</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ManagerPage.qml" line="60"/>
+        <source>About</source>
+        <translation>Over</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ManagerPage.qml" line="71"/>
+        <source>Pebble Manager</source>
+        <translation>Pebble Manager</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ManagerPage.qml" line="78"/>
+        <source>Waiting for watch...
+If it can&apos;t be found please check it&apos;s available and paired in Bluetooth settings.</source>
+        <translation type="unfinished">Wacht op horloge...
+Als hij niet gevonden kan worden, controleer dan of hij beschikbaar en gekoppeld is in de Bluetooth instellingen.</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ManagerPage.qml" line="99"/>
+        <source>Service</source>
+        <translation>Service</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ManagerPage.qml" line="106"/>
+        <source>Enabled</source>
+        <translation>Opstarten</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ManagerPage.qml" line="107"/>
+        <source>Automatic startup</source>
+        <translation>Automatisch opstarten</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ManagerPage.qml" line="107"/>
+        <source>Manual startup</source>
+        <translation>Handmatig opstarten</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ManagerPage.qml" line="113"/>
+        <source>Active</source>
+        <translation>Actief</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ManagerPage.qml" line="114"/>
+        <source>Running</source>
+        <translation>Wordt uitgevoerd</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ManagerPage.qml" line="114"/>
+        <source>Dead</source>
+        <translation>Gestopt</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ManagerPage.qml" line="120"/>
+        <source>Connection</source>
+        <translation>Verbinding</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ManagerPage.qml" line="121"/>
+        <location filename="../qml/pages/ManagerPage.qml" line="259"/>
+        <source>Connected</source>
+        <translation>Verbonden</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ManagerPage.qml" line="121"/>
+        <location filename="../qml/pages/ManagerPage.qml" line="280"/>
+        <source>Disconnected</source>
+        <translation>Niet verbonden</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ManagerPage.qml" line="134"/>
+        <source>Settings</source>
+        <translation>Instellingen</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ManagerPage.qml" line="141"/>
+        <source>Forward phone calls</source>
+        <translation>Telefoonoproepen doorsturen</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ManagerPage.qml" line="150"/>
+        <source>Pebble music volume buttons change the main phone volume directly instead of through the music player</source>
+        <translation>Pebble Music volumeknoppen passen direct het volume van de telefoon aan, in plaats van via de muziekspeler</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ManagerPage.qml" line="251"/>
+        <source>Profiles</source>
+        <translation>Profielen</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ManagerPage.qml" line="262"/>
+        <location filename="../qml/pages/ManagerPage.qml" line="273"/>
+        <location filename="../qml/pages/ManagerPage.qml" line="283"/>
+        <location filename="../qml/pages/ManagerPage.qml" line="294"/>
+        <source>no change</source>
+        <translation>niet aangepast</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ManagerPage.qml" line="149"/>
+        <source>Control main volume</source>
+        <translation>Hoofdvolume bedienen</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ManagerPage.qml" line="158"/>
+        <source>Transliterate messages</source>
+        <translation>Berichten converteren</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ManagerPage.qml" line="159"/>
+        <source>Messages are transliterated to ASCII before sending to Pebble</source>
+        <translation>Berichten worden geconverteerd naar ASCII voordat ze naar de Pebble verzonden worden</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ManagerPage.qml" line="168"/>
+        <source>Notifications</source>
+        <translation>Meldingen</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ManagerPage.qml" line="176"/>
+        <source>Messaging</source>
+        <translation>Berichten</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ManagerPage.qml" line="177"/>
+        <source>SMS and IM</source>
+        <translation>Sms en chat</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ManagerPage.qml" line="186"/>
+        <source>Missed call</source>
+        <translation>Gemiste oproepen</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ManagerPage.qml" line="195"/>
+        <source>Emails</source>
+        <translation>E-mails</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ManagerPage.qml" line="204"/>
+        <source>Mitakuuluu</source>
+        <translation>Mitakuuluu</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ManagerPage.qml" line="213"/>
+        <source>Twitter</source>
+        <translation>Twitter</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ManagerPage.qml" line="223"/>
+        <source>Facebook</source>
+        <translation>Facebook</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ManagerPage.qml" line="232"/>
+        <source>Other notifications</source>
+        <translation>Andere meldingen</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ManagerPage.qml" line="241"/>
+        <source>All notifications</source>
+        <translation>Alle meldingen</translation>
+    </message>
+</context>
+<context>
+    <name>WatchInfo</name>
+    <message>
+        <location filename="../qml/pages/WatchInfo.qml" line="28"/>
+        <source>Address</source>
+        <translation>Adres</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/WatchInfo.qml" line="36"/>
+        <source>Serial Number</source>
+        <translation>Serienummer</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/WatchInfo.qml" line="44"/>
+        <source>BootLoader</source>
+        <translation>Bootloader</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/WatchInfo.qml" line="52"/>
+        <source>Firmware</source>
+        <translation>Firmware</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/WatchInfo.qml" line="69"/>
+        <source>Recovery</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/WatchInfo.qml" line="77"/>
+        <source>Running</source>
+        <translation>In gebruik</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/WatchInfo.qml" line="85"/>
+        <source>Latest</source>
+        <translation>Laatste</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/WatchInfo.qml" line="72"/>
+        <location filename="../qml/pages/WatchInfo.qml" line="80"/>
+        <location filename="../qml/pages/WatchInfo.qml" line="88"/>
+        <source>unknown</source>
+        <translation>onbekend</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/WatchInfo.qml" line="93"/>
+        <source>Upgrade Firmware</source>
+        <translation>Firmware bijwerken</translation>
+    </message>
+</context>
+<context>
+    <name>WatchPage</name>
+    <message>
+        <location filename="../qml/pages/WatchPage.qml" line="34"/>
+        <source>Info</source>
+        <translation>Info</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/WatchPage.qml" line="40"/>
+        <source>Ping</source>
+        <translation>Ping</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/WatchPage.qml" line="46"/>
+        <source>Sync Time</source>
+        <translation>Tijd syncen</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/WatchPage.qml" line="78"/>
+        <source>Installed applications</source>
+        <translation>Geïnstalleerde applicaties</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/WatchPage.qml" line="68"/>
+        <source>Your firmware is too old to support SDKv2 applications</source>
+        <translation>Uw firmware is te oud om SDKv2-applicaties te ondersteunen</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/WatchPage.qml" line="116"/>
+        <source>Uninstalling</source>
+        <translation>Bezig met de-installeren</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/WatchPage.qml" line="188"/>
+        <source>(empty slot)</source>
+        <translation>(leeg vak)</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/WatchPage.qml" line="188"/>
+        <source>(slot in use by unknown app)</source>
+        <translation>(vak gebruikt door onbekende app)</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/WatchPage.qml" line="197"/>
+        <source>Install app...</source>
+        <translation>App installeren...</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/WatchPage.qml" line="202"/>
+        <source>Companion app missing</source>
+        <translation>Bijbehorende app ontbreekt</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/WatchPage.qml" line="215"/>
+        <source>Configure...</source>
+        <translation>Instellen...</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/WatchPage.qml" line="220"/>
+        <source>Uninstall</source>
+        <translation>De-installeren</translation>
+    </message>
+</context>
+</TS>
